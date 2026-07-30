c06d045a59d6e01b16723f2c1d9b38dbf1f2f835
krishna manglik <krishnamanglik2504@gmail.com>
2026-07-30 11:36:35 +0530

    UD sir

---
+++ day1/index.js
@@ -0,0 +1,37 @@
+const app = require("./app");
+
+
+
+var a = 10;
+var b = 10;
+var c = 710;
+console.log(a+b+c);
+
+var x=20;
+if (x==20)
+{
+    console.log("x is 20");
+    
+}  
+else{
+    console.log("x is not 20");
+    
+}
+    
+
+for(var i=0;i<5;i++){
+    console.log(i);
+    
+}
+
+
+const arr=[1,2,3,4,5];
+for(var i = 0 ; i < arr.length;i++){
+   console.log(arr[i]);
+}
+
+console.log(app.xz);
+console.log(app.yy);
+
+
+
