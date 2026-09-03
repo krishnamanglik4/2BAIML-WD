826464496a19a222729142dcf50dc48a03189e63
krishna manglik <krishnamanglik2504@gmail.com>
2026-09-03 11:23:39 +0530

    Arrow Function

--- day4/script.js
+++ day4/script.js
@@ -3,3 +3,11 @@ let lname = "Manglik";
 console.log("My First Name is " + fname + " and My Last Name is " + lname);
 console.log(`My Mirst Name is ${fname} and My Last Name is ${lname}`);
 console.log(`My Name is ${fname} ${lname}`);
+
+
+function fullname(first , last){
+        return `${first}${last}`;
+}
+let name = `Hello ${fullname(fname , lname)}`;
+
+console.log(name);
