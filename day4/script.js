6873dabd74324b6127ebb2fd26130e85e2f1cdb1
krishna manglik <krishnamanglik2504@gmail.com>
2026-09-03 11:01:12 +0530

    string template

---
+++ day4/script.js
@@ -0,0 +1,7 @@
+let fname = "Krishna";
+let lname = "Manglik";
+console.log("My First Name is " + fname + " and My Last Name is " + lname);
+console.log(`My Mirst Name is ${fname} and My Last Name is ${lname}`);
+console.log(`My Name is ${fname} ${lname}`);
+
+
